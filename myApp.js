2d690218bc6e3a1385463a516419564673e29de5
Rafael Uen <rafael.uen@liferay.com>
2023-04-04 19:39:22 -0300

    add /json

--- myApp.js
+++ myApp.js
@@ -6,6 +6,10 @@ app.use('/public', express.static(__dirname+'/public'))
 app.get('/', (req, res) => {
     res.sendFile(__dirname +'/views/index.html')
 })
+
+app.get('/json', (req, res) => {
+    res.json({'message': 'Hello json'})
+})
 console.log('Hello World')
 
 
