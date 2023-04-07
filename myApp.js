e2257fdd9ce95df3ec138815e2ab8346b57f8789
Rafael Uen <rafael.uen@liferay.com>
2023-04-07 14:30:46 -0300

    starting new day commit

--- myApp.js
+++ myApp.js
@@ -1,16 +1,23 @@
 let express = require('express');
 let app = express();
+let dotenv = require('dotenv').config()
 
 app.use('/public', express.static(__dirname+'/public'))
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname +'/views/index.html')
+    console.log('entered /')
+    if (process.env.VAR_NAME === "allCaps") {
+        response = "Hello World".toUpperCase();
+      } else {
+        response = "Hello World";
+      }
 })
 
 app.get('/json', (req, res) => {
     res.json({'message': 'Hello json'})
+    console.log('enetered /json')
 })
-console.log('Hello World')
 
 
 
